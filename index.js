f340ab5ddda23a4aa41d302b4c8413b6b5a18c13
vinyborges <viny.bdj@gmail.com>
2023-06-09 19:15:14 -0700

    commit tree d3d23fb22f74f5e5163edc76c2f0fc4e5cfbdc20

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const surprise=[];
+function writeCards(names,surprise) {
+ 
+  for (let i = 0; i < names.length; i++) {
+   
+
+ surprise.push(`Thank you,${names[i]} , for the wonderful surprise gift!`);
+   
+  } 
+  return surprise;
+}
+
+writeCards(names,surprise);
+
+function countDown(){
+  let countdown = 10;
+while (countdown >=0) {
+  console.log(countdown--);
+}
+}
